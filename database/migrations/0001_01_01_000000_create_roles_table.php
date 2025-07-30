aaa0e1d277b699f0f71496e39b28e7eece1f9457
fadiel4721 <fadielintip@gmail.com>
2025-07-30 13:22:47 +0700

    refactor(feature-role) : - merefactoring migration roles

---
+++ database/migrations/0001_01_01_000000_create_roles_table.php
@@ -0,0 +1,27 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Support\Facades\DB;
+
+return new class extends Migration {
+    public function up() {
+        Schema::create('roles', function (Blueprint $table) {
+            $table->id();
+            $table->string('name')->unique(); // nama: admin, penerbit, user
+            $table->timestamps();
+        });
+
+        // Seed default roles
+        DB::table('roles')->insert([
+            ['name' => 'admin'],
+            ['name' => 'penerbit'],
+            ['name' => 'user'],
+        ]);
+    }
+
+    public function down() {
+        Schema::dropIfExists('roles');
+    }
+};
